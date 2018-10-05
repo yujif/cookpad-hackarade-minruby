d0fa5fdc8313907ad52146ce76275a503ae4873b
Yuji Fujisaka <yuji-fujisaka@cookpad.com>
2018-10-05 11:37:26 +0900

    Implement `if` and `while`

--- interp.rb
+++ interp.rb
@@ -24,6 +24,12 @@ def evaluate(exp, env)
     evaluate(exp[1], env) % evaluate(exp[2], env)
   when "/"
     evaluate(exp[1], env) / evaluate(exp[2], env)
+  when ">"
+    evaluate(exp[1], env) > evaluate(exp[2], env)
+  when "<"
+    evaluate(exp[1], env) < evaluate(exp[2], env)
+  when "=="
+    evaluate(exp[1], env) == evaluate(exp[2], env)
   
 #
 ## Problem 2: Statements and variables
@@ -71,12 +77,17 @@ def evaluate(exp, env)
     #   else
     #     ???
     #   end
-    raise(NotImplementedError) # Problem 3
-
+    if evaluate(exp[1], env)
+      evaluate(exp[2], env) 
+    else
+      evaluate(exp[3], env) 
+    end
+  
   when "while"
     # Loop.
-    raise(NotImplementedError) # Problem 3
-
+    while evaluate(exp[1], env)
+      evaluate(exp[2], env)
+    end
 
 #
 ## Problem 4: Function calls
